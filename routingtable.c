42d836c048aa1774509ce94ce086818fd68ee5d6
Mike <wiggles187@gmail.com>
2019-09-13 13:25:24 +0200

    Changed some variable names to be more legible

--- routingtable.c
+++ routingtable.c
@@ -14,27 +14,27 @@ uint8_t aRoutingTable[256][32];													//256 arrays with 256 bits, position
 																				//bits used as boolean (is ID neighbor?)
 uint8_t uMyID;
 
-void copyrow( uint8_t idrow, uint8_t* row ){									//Copies neighbor list received from neighbor to table
-	if ( sizeof(row) == 32 ) memcpy( aRoutingTable[idrow], row , 32);
+void copyrow( uint8_t uIDRow, uint8_t* aRow ){									//Copies neighbor list received from neighbor to table
+	if ( sizeof(aRow) == 32 ) memcpy( aRoutingTable[uIDRow], aRow , 32);
 }
 
-void addneighbor( uint8_t idneighbor ){											//Function for adding own neighbor to table
-	aRoutingTable[uMyID][idneighbor / 8] |= 0x01 << (idneighbor % 8);
+void addneighbor( uint8_t uIDNeighbor ){											//Function for adding own neighbor to table
+	aRoutingTable[uMyID][uIDNeighbor / 8] |= 0x01 << (uIDNeighbor % 8);
 }
 
-void removeneighbor( uint8_t idneighbor ){										//Function for removing own neighbor from table
-	aRoutingTable[uMyID][idneighbor / 8] &= ~(0x01 << (idneighbor % 8));
+void removeneighbor( uint8_t uIDNeighbor ){										//Function for removing own neighbor from table
+	aRoutingTable[uMyID][uIDNeighbor / 8] &= ~(0x01 << (uIDNeighbor % 8));
 }
 
 uint8_t* getownlist( void ){													//Returns a pointer to own neighbor list for sending to neighbor
 	return aRoutingTable[uMyID];
 }
 
-uint8_t* getotherlist( uint8_t idsensornode ){									//Returns pointer to neighbor list of other node for sending to neighbor
-	return aRoutingTable[idsensornode];
+uint8_t* getotherlist( uint8_t uIDSensorNode ){									//Returns pointer to neighbor list of other node for sending to neighbor
+	return aRoutingTable[uIDSensorNode];
 }
 
 void init_routingtable( uint8_t uNodeID ){
 	uMyID = uNodeID;
-	for (uint8_t i = 0; i <= 255; i++) memset(aRoutingTable, 0, 32);
+	for (uint8_t i = 0; i <= 255; i++) memset(aRoutingTable[i], 0, 32);
 }
--- routingtable.h
+++ routingtable.h
@@ -9,11 +9,11 @@
 #ifndef ROUTINGTABLE_H_
 #define ROUTINGTABLE_H_
 
-void copyrow( uint8_t idrow, uint8_t* row );
-void addneighbor( uint8_t idneighbor );
-void removeneighbor( uint8_t idneighbor );
+void copyrow( uint8_t uIDRow, uint8_t* aRow );
+void addneighbor( uint8_t uIDNeighbor );
+void removeneighbor( uint8_t uIDNeighbor );
 uint8_t* getownlist( void );
-uint8_t* getotherlist( uint8_t idsensornode );
+uint8_t* getotherlist( uint8_t uIDSensorNode );
 void init_routingtable( uint8_t uNodeID );
 
 #endif /* ROUTINGTABLE_H_ */
