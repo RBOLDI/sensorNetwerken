3c2001bb7df21f09bedfb1a92a1c967dd90f7c4a
Stephan <stephan.van.bottenburg@hva.nl>
2019-09-30 16:29:29 +0200

    Finished receive funtions and checked for errors
    Not yet tested

--- networkComm.h
+++ networkComm.h
@@ -2,7 +2,7 @@
  * networkComm.h
  *
  * Created: 30-9-2019 12:24:22
- *  Author: Rowan
+ *  Author: RBOLDI & YaBoii Steph
  */ 
 
 
@@ -11,9 +11,8 @@
 
 void init_PrivateComm(uint8_t _myid);
 void sendPrivateMSG (uint8_t targetID, uint8_t *data);
-
+uint8_t isMine(uint8_t MyID, uint8_t *data);
+void ReceiveData(uint8_t MyID, uint8_t *data);
 
 #endif /* NETWORKCOMM_H_ */
 
-uint8_t isMine(uint8_t MyID, uint8_t *data);
-ReceiveData(uint8_t MyID, uint8_t *data);
