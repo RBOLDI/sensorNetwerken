18d9a5ce0e26f5509f477cb4c25385dfc48b6c97
RBOLDI <Rowan.Bolding@hva.nl>
2019-09-30 12:24:51 +0200

    Added communication files

---
+++ networkComm.c
@@ -0,0 +1,6 @@
+/*
+ * networkComm.c
+ *
+ * Created: 30-9-2019 12:23:56
+ *  Author: Rowan
+ */ 
---
+++ networkComm.h
@@ -0,0 +1,16 @@
+/*
+ * networkComm.h
+ *
+ * Created: 30-9-2019 12:24:22
+ *  Author: Rowan
+ */ 
+
+
+#ifndef NETWORKCOMM_H_
+#define NETWORKCOMM_H_
+
+
+
+
+
+#endif /* NETWORKCOMM_H_ */
