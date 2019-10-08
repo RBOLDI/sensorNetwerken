d9f2624cc1642d6c0a0f2591df2b3e989d97fd26
Felix <felixvanbeusekom@live.nl>
2019-10-08 12:38:37 +0200

    Removed libraries that were not needed

--- messages.h
+++ messages.h
@@ -5,19 +5,10 @@
  *  Author: Steph
  */ 
 
-// Defines used by message functions.
-#define		FULL_MESSAGE_SIZE 32
-#define		NUMBER_OF_PREFIX_BYTES 3
-#define		MAX_MESSAGE_SIZE FULL_MESSAGE_SIZE - NUMBER_OF_PREFIX_BYTES // Waarvan de laatste is '\0'
-
 #define		BROADCAST	0x01
 #define		RHDR		0x02
 #define		DHDR		0x03
 #define		BCREPLY		0x04
-
-// Integers shared by message functions.
-uint8_t message[MAX_MESSAGE_SIZE];
-uint8_t fullMessage[FULL_MESSAGE_SIZE];
 
 //Function protoypes.
 uint8_t* pipe_selector(uint8_t ID);
